d8fee4633bd5ad55660e4d8558f1490ffbe02cfa
bmindaugas <37580537+bmindaugas@users.noreply.github.com>
2018-05-02 20:22:04 +0300

    antras kiek suprantu

---
+++ naujas2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+        
+    <title>PHP</title>
+
+</head>
+<body>
+    <?php echo "<h1>Antraštė</h1>"?>     
+    <input type="text">
+    <label for="name">Vardas</label>
+    <input type="password">
+    <label for="psw">Slaptazodis</label>
+    <button type="submit">Prisijungti</button>    
+    <center>&copy; 2010-<?php echo date("Y");?></center>
+</body>
+</html>
